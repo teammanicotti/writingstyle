8157b12927b5e4c13d694fcf5dc0e310897e691d
rl5849 <rl5849@rit.edu>
2019-12-01 16:19:53 -0500

    Fixed incorrect paragraph index reporting from passive to active and sentiment reversal. Added UUID to recommendations for the client side.

--- seniorproject/model/recommendation.py
+++ seniorproject/model/recommendation.py
@@ -2,7 +2,8 @@
 from typing import List
 
 from seniorproject.model.recommendationtype import RecommendationType
-
+import uuid
+import random
 
 class Recommendation:
     """Represents a recommendation made by a recommendation engine."""
@@ -22,7 +23,7 @@ class Recommendation:
             end_offset: int,
             paragraph_index: int,
             new_values: List[str],
-            confidence: float = None
+            confidence: float = None,
     ):
         self.recommendation_type = recommendation_type
         self.original_text = original_text
@@ -31,6 +32,7 @@ class Recommendation:
         self.paragraph_index = paragraph_index
         self.new_values = new_values
         self.confidence = confidence
+        self.uuid = str(uuid.uuid4())
 
     def to_json(self):
         """Creates a JSON-compatible representation of the recommendation."""
