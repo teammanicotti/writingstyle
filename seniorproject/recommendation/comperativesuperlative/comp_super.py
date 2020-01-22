bb8824917a909be1528cec6aee7401dca620fd78
Ben Christians <bbc7909@rit.edu>
2020-01-22 11:14:45 -0500

    Fixed some style issues

--- seniorproject/recommendation/comperativesuperlative/comp_super.py
+++ seniorproject/recommendation/comperativesuperlative/comp_super.py
@@ -1,22 +1,30 @@
+"""
+Comparative and Superlative:
+
+Contains a method to annotate a sentence with improvements to
+comparative and superlative usage
+"""
 from typing import List
+from pattern.en import comparative, superlative
 
 from seniorproject.model.document import Document
 from seniorproject.model.recommendation import Recommendation
 from seniorproject.model.recommendationtype import RecommendationType
 from seniorproject.recommendation.recommendationengine import RecommendationEngine
 
-from pattern.en import comparative, superlative
-
-pos_with_comp_super = ('ADJ', 'ADV')
+POS_WITH_COMP_SUPER = ('ADJ', 'ADV')
 
 
 def annotate_sentence(sentence, index):
+    """
+    Returns a list of recommendations based on an input sentence
+    """
     improvements: List[Recommendation] = []
 
     for word in sentence:
         if word.text.lower() == "more":
             head = word.head
-            if head.pos_ in pos_with_comp_super:
+            if head.pos_ in POS_WITH_COMP_SUPER:
                 new_comparative: str = comparative(head.text.lower())
                 if "more" not in new_comparative:
                     improvements.append(
@@ -33,7 +41,7 @@ def annotate_sentence(sentence, index):
 
         if word.text.lower() == "most":
             head = word.head
-            if head.pos_ in pos_with_comp_super:
+            if head.pos_ in POS_WITH_COMP_SUPER:
                 new_superlative: str = superlative(head.text.lower())
                 if "most" not in new_superlative:
                     improvements.append(
@@ -51,9 +59,10 @@ def annotate_sentence(sentence, index):
 
 
 class ComparativeSuperlativeAnalyzer(RecommendationEngine):
-
-    def __init__(self):
-        super(ComparativeSuperlativeAnalyzer, self).__init__()
+    """
+    Class which maintains logic to provide recommendations on comparative and
+    superlative usage
+    """
 
     def analyze(self, doc: Document) -> List[Recommendation]:
 
