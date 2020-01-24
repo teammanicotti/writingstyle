ea9a2e8d867473e0a12f73dc7ba283d49f30bebe
Devon Welcheck <dw5005@rit.edu>
2020-01-24 15:07:25 -0500

    Fixed model import in spacy_extensions test

--- tests/preprocessing/test_spacy_extensions.py
+++ tests/preprocessing/test_spacy_extensions.py
@@ -1,4 +1,4 @@
-import en_core_web_sm
+import en_core_web_lg
 import pytest
 
 from preprocessing import spacy_extensions
@@ -6,7 +6,7 @@ from preprocessing import spacy_extensions
 
 @pytest.fixture
 def nlp():
-    spacy_instance = en_core_web_sm.load()
+    spacy_instance = en_core_web_lg.load()
     spacy_extensions.enable_spacy_extensions()
     spacy_instance.add_pipe(
         spacy_extensions.retokenize_citations,
