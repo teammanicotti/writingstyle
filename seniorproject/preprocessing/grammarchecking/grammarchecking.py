930d84817b736044d1fba94d2837fff8ba501249
Doanh Pham <doanhpham1995@gmail.com>
2020-01-31 20:19:51 -0500

    updated grammar checking

--- seniorproject/preprocessing/grammarchecking/grammarchecking.py
+++ seniorproject/preprocessing/grammarchecking/grammarchecking.py
@@ -1,8 +1,9 @@
 import requests
 from spellchecker import SpellChecker
-from spacy.tokens import Span
+from spacy.tokens import Span, Token
 
 
+# noinspection PyProtectedMember
 class GrammarChecking:
 
     def __init__(self, spacy_instance, stop_words=None):
@@ -11,46 +12,68 @@ class GrammarChecking:
         self.whitelist_words = {'pajamas'}
         self.whitelist_grammar = {'UPPERCASE_SENTENCE_START'}
         self.spell = SpellChecker()
-        Span.set_extension('is_invalid_sentence', default=False, force=True)
 
-    def check_sentence_spell(self, sentence):
+        Span.set_extension('has_grammar_errors', default=False, force=True)
+        Span.set_extension('grammar_recommendation', default=[], force=True)
+
+        Span.set_extension('has_spelling_errors', default=False, force=True)
+        Token.set_extension('correct_spelling_candidates', default=[], force=True)
+
+    def check_sentence_has_spelling_error(self, sentence):
         """
-        Checks spelling in a sentence
+        Checks spelling in a sentence and set token with correct spelling candidates if it
+        has spelling error
         :param sentence: Doc
         :return: boolean
         """
-        words = []
+        has_spelling_error = False
         for token in sentence:
             if token.text not in self.stop_words and token.text.lower() not in self.whitelist_words:
-                words.append(token.text)
-        wrong_spells = self.spell.unknown(words)
-        return len(wrong_spells) == 0
+                wrong_spells = self.spell.unknown([token.text])
+                if len(wrong_spells) > 0:
+                    has_spelling_error = True
+                    token._.set('correct_spelling_candidates', self.spell.candidates(token.text))
+        return has_spelling_error
 
-    def check_sentence_grammar(self, sentence):
+    def check_sentence_has_grammar_error(self, sentence):
         """
-        Checks grammar of a sentence
+        Checks grammar of a sentence. If it has grammar errors, the function will return
+        True and the recommendation to fix it
         :param sentence: str
         :return: boolean
         """
         params = [('language', 'en'), ('text', sentence)]
-        response = requests.get('http://grammar:8081/v2/check', params)
+        response = requests.get('http://localhost:8081/v2/check', params)
         response_json = response.json()
         matches = response_json['matches']
+        recommendation = []
+        has_grammar_errors = False
+
         if len(matches) == 1 and matches[0]['rule']['id'] in self.whitelist_grammar:
-            return True
-        return len(matches) == 0
+            return {"has_grammar_errors": has_grammar_errors, "recommendation": recommendation}
+
+        if len(matches) > 0:
+            has_grammar_errors = True
+            for match in matches:
+                recommendation.append({ "message": match["message"], "context": match["context"] });
+
+        return {"has_grammar_errors": has_grammar_errors, "recommendation": recommendation}
 
     def check_sentences(self, sentences):
         """
-        Checks all sentences and return spacy doc with custom Span that has attribute is_invalid_sentence.
-        If a sentence has either grammar error or spelling, the is_invalid_sentence will be True. You can
-        access this attribute by sentence._.is_invalid_sentence
+        Checks all sentences and return the Spacy doc with new extension so you can check
+        if a Spacy span has spelling or grammar errors and the fixing recommendation
         :param sentences: str
-        :return: Doc
+        :return: doc
         """
+        results = []
         doc = self.nlp(sentences)
         for sentence in doc.sents:
-            if not (self.check_sentence_spell(sentence) and self.check_sentence_grammar(sentence.text)):
-                # noinspection PyProtectedMember
-                sentence._.set("is_invalid_sentence", True)
+            grammar_result = self.check_sentence_has_grammar_error(sentence.text)
+            sentence._.set("has_grammar_errors", grammar_result["has_grammar_errors"])
+            sentence._.set("grammar_recommendation", grammar_result["recommendation"])
+
+            if self.check_sentence_has_spelling_error(sentence):
+                sentence._.set("has_spelling_errors", True)
+
         return doc
--- tests/preprocessing/grammarchecking/test_grammarchecking.py
+++ tests/preprocessing/grammarchecking/test_grammarchecking.py
@@ -7,24 +7,60 @@ grammar = GrammarChecking(nlp)
 
 # noinspection PyProtectedMember
 def test_sentences_that_contains_sentence_has_spelling_error():
-    sentences = "One morning I shot an elephant in my pajamas. How he got intto my pajamas I'll never know."
+    sentences = "One morning I shot an elephant in my pajamas. How he got intto my pajamas? I'll never know."
     doc = grammar.check_sentences(sentences)
+
+    index = 0
     for sentence in doc.sents:
-        if sentence.text is "One morning I shot an elephant in my pajamas.":
-            assert sentence._.is_invalid_sentence is False
-        elif sentence.text is "How he got intto my pajamas I'll never know.":
-            assert sentence._.is_invalid_sentence is True
+        if index == 0:
+            assert sentence._.has_grammar_errors is False
+            assert sentence._.has_spelling_errors is False
+        elif index == 1:
+            assert sentence._.has_grammar_errors is False
+            assert sentence._.has_spelling_errors is True
+            for token in sentence:
+                if token.text == "intto":
+                    assert len(token._.correct_spelling_candidates) > 0
+                else:
+                    assert len(token._.correct_spelling_candidates) == 0
+        elif index == 2:
+            assert sentence._.has_grammar_errors is False
+            assert sentence._.has_spelling_errors is False
+        index += 1
 
 
 # noinspection PyProtectedMember
 def test_sentences_that_contains_sentence_has_grammar_error():
     sentences = "The food was eat by the man. The food was eaten by John."
     doc = grammar.check_sentences(sentences)
+
+    index = 0
     for sentence in doc.sents:
-        if sentence.text is "The food was eat by the man.":
-            assert sentence._.is_invalid_sentence is True
-        elif sentence.text is "The food was eaten by John.":
-            assert sentence._.is_invalid_sentence is False
+        if index == 0:
+            assert sentence._.has_grammar_errors is True
+            assert len(sentence._.grammar_recommendation) > 0
+            assert sentence._.has_spelling_errors is False
+        elif index == 1:
+            assert sentence._.has_grammar_errors is False
+            assert sentence._.has_spelling_errors is False
+        index += 1
+
+
+# noinspection PyProtectedMember
+def test_sentences_that_contains_sentence_has_grammar_error_and_spelling_error():
+    sentences = "The food was eat by the mcn. The food was eaten by John."
+    doc = grammar.check_sentences(sentences)
+
+    index = 0
+    for sentence in doc.sents:
+        if index == 0:
+            assert sentence._.has_grammar_errors is True
+            assert len(sentence._.grammar_recommendation) > 0
+            assert sentence._.has_spelling_errors is True
+        elif index == 1:
+            assert sentence._.has_grammar_errors is False
+            assert sentence._.has_spelling_errors is False
+        index += 1
 
 
 # noinspection PyProtectedMember
@@ -32,25 +68,30 @@ def test_sentences_that_contains_all_valid_sentence():
     sentences = "Mark went to the store. His mom did not have food for dinner."
     doc = grammar.check_sentences(sentences)
     for sentence in doc.sents:
-        assert sentence._.is_invalid_sentence is False
+        assert sentence._.has_grammar_errors is False
+        assert len(sentence._.grammar_recommendation) == 0
+        assert sentence._.has_spelling_errors is False
 
 
 def test_sentence_has_spelling_error():
     sentence = "The rat the ccat the dog chased killed ate the malt."
-    assert grammar.check_sentence_spell(nlp(sentence)) is False
+    assert grammar.check_sentence_has_spelling_error(nlp(sentence)) is True
 
 
 def test_sentence_not_have_spelling_error():
     sentence = "The horse raced past the barn fell."
-    assert grammar.check_sentence_spell(nlp(sentence)) is True
+    assert grammar.check_sentence_has_spelling_error(nlp(sentence)) is False
 
 
 def test_sentence_has_grammar_error():
     sentence = "The boy was was grumpy."
-    assert grammar.check_sentence_grammar(sentence) is False
+    result = grammar.check_sentence_has_grammar_error(sentence)
+    assert result["has_grammar_errors"] is True
+    assert len(result["recommendation"]) > 0
 
 
 def test_sentence_not_have_grammar_error():
     sentence = "The food was eaten by John."
-    assert grammar.check_sentence_grammar(sentence) is True
-
+    result = grammar.check_sentence_has_grammar_error(sentence)
+    assert result["has_grammar_errors"] is False
+    assert len(result["recommendation"]) == 0
