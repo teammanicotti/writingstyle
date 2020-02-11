e2a2e5939cc1bc0ace006c288762f9be724d9ded
Devon Welcheck <dw5005@rit.edu>
2020-02-10 21:44:00 -0500

    Let's try scoping the similarityclassifier fixture to session to reduce RAM

--- tests/recommendation/simpletocompound/semanticsimilarity/test_similarityclassifier.py
+++ tests/recommendation/simpletocompound/semanticsimilarity/test_similarityclassifier.py
@@ -7,9 +7,43 @@ from seniorproject.recommendation.simpletocompound.semanticsimilarity import \
     similarityclassifier
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def similarity_classifier():
-    session, encodings, input_placeholder, sentence_piece_processor = init_tf()
+    graph = tf.compat.v1.Graph()
+    with graph.as_default():  # pylint: disable=not-context-manager
+        input_placeholder = tf.compat.v1.sparse_placeholder(
+            tf.int64,
+            shape=[None, None]
+        )
+        module = hub.Module(
+            "https://tfhub.dev/google/universal-sentence-encoder-lite/2"
+        )
+        init_op = tf.group(
+            [
+                tf.compat.v1.global_variables_initializer(),
+                tf.compat.v1.tables_initializer()
+            ]
+        )
+        encodings = module(
+            inputs=dict(
+                values=input_placeholder.values,
+                indices=input_placeholder.indices,
+                dense_shape=input_placeholder.dense_shape
+            )
+        )
+
+        session = tf.compat.v1.Session(graph=graph)
+        spm_path = session.run(module(signature="spm_path"))
+        graph.finalize()
+        session.run(init_op)
+        sentence_piece_processor = spm.SentencePieceProcessor()
+        sentence_piece_processor.Load(spm_path)
+
+    # Reduce logging output.
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+    # return session, encodings, input_placeholder, sentence_piece_processor
+    # session, encodings, input_placeholder, sentence_piece_processor = init_tf()
     return similarityclassifier.SimilarityClassifier(
         session,
         encodings,
